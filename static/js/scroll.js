8a60cffac8bccb3923420148496ef071feac1bd0
Daren Liang <darenliang@outlook.com>
2019-12-11 18:18:34 -0500

    Add css and js for project page

---
+++ static/js/scroll.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+    $("body").css({
+        "transition": "background-color 1s ease"
+    })
+});
+
+$(window).scroll(function () {
+
+    // selectors
+    var $window = $(window),
+        $body = $('body'),
+        $panel = $('.panel');
+
+    // Change 33% earlier than scroll position so colour is there when you arrive.
+    var scroll = $window.scrollTop() + ($window.height() / 3);
+
+    $panel.each(function () {
+        var $this = $(this);
+
+        // if position is within range of this panel.
+        // So position of (position of top of div <= scroll position) && (position of bottom of div > scroll position).
+        // Remember we set the scroll to 33% earlier in scroll var.
+        if ($this.position().top <= scroll && $this.position().top + $this.height() > scroll) {
+
+            // Remove all classes on body with color-
+            $body.removeClass(function (index, css) {
+                return (css.match(/(^|\s)color-\S+/g) || []).join(' ');
+            });
+
+            // Add class of currently active div
+            $body.addClass('color-' + $(this).data('color'));
+        }
+    });
+
+}).scroll();
