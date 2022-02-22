dfad4ce97686ed0e1df51f5b55533848c0df7184
Daren Liang <daren.liang@mail.utoronto.ca>
2022-02-22 01:48:10 -0500

    Small fix

--- static/js/externs.js
+++ static/js/externs.js
@@ -2,4 +2,9 @@
  * We store a list of externs here to make sure Closure Compiler doesn't complain when on ADVANCED
  */
 
-var script;
+
+// ytdl-ffmpeg-demo-controller.js
+const script = {};
+script.getVideoBuffer = function(url) {};
+script.getEncodedVideoBuffer = function(url) {};
+script.getEncodedAudioBuffer = function(url) {};
